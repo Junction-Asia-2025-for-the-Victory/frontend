c4f1f7b6056068f9c8751e9b2766cf1b70a17dae
kub938 <kub938@naver.com>
2025-08-23 18:56:14 +0900

    feat: router 로그인 제한  구현

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,68 +1,55 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
 import App from "../App";
 import Login from "../pages/Login/Login";
 // import Home from "../pages/Home/Home";
+import { ProtectedRoute } from "./RequireAuth";
+import { RequireGuest } from "./RequireGuest";
+import ErrorPage from "../components/ErrorPage";
 import EpisodeList from "@/pages/Episode/Episode";
-import Play from "@/pages/Play/Play";
 import Entry from "@/pages/Entry/Entry";
-// import { ProtectedRoute } from "./RequireAuth";
-// import { RequireGuest } from "./RequireGuest";
-// import ErrorPage from "../components/ErrorPage";
+import Play from "@/pages/Play/Play";
 
 // 로그인 상태에 따른 페이지 접근권한 컨트롤
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         element: (
-//           <ProtectedRoute>
-//             <Outlet />
-//           </ProtectedRoute>
-//         ),
-//         children: [{ path: "", element: <Home /> }],
-//       },
-//       {
-//         element: (
-//           <RequireGuest>
-//             <Outlet />
-//           </RequireGuest>
-//         ),
-//         children: [
-//           { path: "login", element: <Login /> },
-//           { path: "", element: <Home /> },
-//         ],
-//       },
-//     ],
-//   },
-// ]);
-
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
-        element: <EpisodeList />,
-        path: "",
+        element: (
+          <ProtectedRoute>
+            <Outlet />
+          </ProtectedRoute>
+        ),
+        children: [
+          {
+            element: <EpisodeList />,
+            path: "",
+          },
+          {
+            element: <EpisodeList />,
+            path: "episode",
+          },
+          {
+            element: <Play />,
+            path: "play",
+          },
+        ],
       },
       {
-        element: <Login />,
-        path: "login",
-      },
-      {
-        element: <EpisodeList />,
-        path: "episode",
-      },
-      {
-        element: <Play />,
-        path: "play",
-      },
-      {
-        element: <Entry />,
-        path: "entry",
+        element: (
+          <RequireGuest>
+            <Outlet />
+          </RequireGuest>
+        ),
+        children: [
+          { path: "login", element: <Login /> },
+          {
+            element: <Entry />,
+            path: "entry",
+          },
+        ],
       },
     ],
   },
